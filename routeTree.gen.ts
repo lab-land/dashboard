27cd42196df4e7e3abed3f09fcd2ea0c21623b86
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-02-22 23:17:16 -0500

    Add new route products

--- routeTree.gen.ts
+++ routeTree.gen.ts
@@ -11,9 +11,15 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as ProductsImport } from './routes/products'
 import { Route as IndexImport } from './routes/index'
 
 // Create/Update Routes
+
+const ProductsRoute = ProductsImport.update({
+  path: '/products',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const IndexRoute = IndexImport.update({
   path: '/',
@@ -28,11 +34,15 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/products': {
+      preLoaderRoute: typeof ProductsImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren([IndexRoute])
+export const routeTree = rootRoute.addChildren([IndexRoute, ProductsRoute])
 
 /* prettier-ignore-end */
