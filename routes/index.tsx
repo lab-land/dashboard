f9d7c5e94063bdeb834680bb0639925dbf6a533a
santiagoRicops <santuago231@gmail.com>
2024-02-28 15:39:28 -0500

    🚀 The top menu of the dashboard was created (#8)

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,22 @@
-import Button from "@/components/Button";
-import { useCounter } from "@/stores/counter";
-import { createFileRoute } from "@tanstack/react-router";
-import { CornerDownLeft } from "lucide-react";
+import Button from '@/components/Button'
 
-export const Route = createFileRoute("/")({
+import { useCounter } from '@/stores/counter'
+import { createFileRoute } from '@tanstack/react-router'
+import { CornerDownLeft } from 'lucide-react'
+
+export const Route = createFileRoute('/')({
   component: Home,
-});
+})
 
 function Home() {
-  const counter = useCounter((state) => state.counter);
+  const counter = useCounter((state) => state.counter)
   return (
-    <div className="p-2">
+    <div>
       <h3>Welcome Home! TanStack Router</h3>
       <p>{counter}</p>
       <Button />
-      <div className="i-logos-vue text-3xl" />
-      <CornerDownLeft color="red"/>
+      <div className='i-logos-vue text-3xl' />
+      <CornerDownLeft color='red' />
     </div>
-  );
+  )
 }
